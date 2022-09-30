da2721dac22526d57d285842f63e90142421855e
jiaoji1 <jiaoji1@staff.weibo.com>
2022-09-30 11:34:34 +0800

    :tada: first version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jiaoji100/slices
+
+go 1.19
---
+++ slices.go
@@ -0,0 +1,59 @@
+// Package slices
+// Author: 919965111@qq.com
+// Date: 2022/9/30 10:12
+// Description:  slice greater functions
+package slices
+
+func ContainsInt(src []int, sub int) bool {
+	for _, v := range src {
+		if v == sub {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsInt32(src []int32, sub int32) bool {
+	for _, v := range src {
+		if v == sub {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsInt64(src []int64, sub int64) bool {
+	for _, v := range src {
+		if v == sub {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsFloat32(src []float32, sub float32) bool {
+	for _, v := range src {
+		if v == sub {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsFloat64(src []float64, sub float64) bool {
+	for _, v := range src {
+		if v == sub {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsString(src []string, sub string) bool {
+	for _, v := range src {
+		if v == sub {
+			return true
+		}
+	}
+	return false
+}
